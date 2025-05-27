a90f428ef009543b2e924aab866fbc63abcd2016
Gaurav Sharma <gauraxvx0410@gmail.com>
2025-05-27 20:44:43 +0530

    post

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,13 +2,17 @@ const {Pool} = require('pg')
 const dotenv = require('dotenv')
 dotenv.config()
 
+// const pool = new Pool({
+//     user: process.env.user,
+//     host: process.env.host,
+//     database: process.env.database,
+//     password: process.env.password,
+//     port: process.env.port
+// })
+
 const pool = new Pool({
-    user: process.env.user,
-    host: process.env.host,
-    database: process.env.database,
-    password: process.env.password,
-    port: process.env.port
-})
+  connectionString: process.env.DATABASE_URL,
+});
 
 
 
